ed6d9ddf6607a7248385d974f3e5ea1fd81a6c75
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-01-26 15:29:03 +0100

    completato

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style scoped>
+.hero {
+  background: url("../assets/img/hero.jpg") no-repeat center;
+  background-size: cover;
+  height: 500px;
+  width: 100%;
+}
+</style>
